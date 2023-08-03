a84434f49e761afee6d6bca0dff91210f428e3ca
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-08-04 01:24:30 +0600

    change style of menu items

--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -30,10 +30,10 @@ const Menu = () => {
     return (
         <div className=' min-h-screen'>
             <BackButton></BackButton>
-            <div className='mt-4 ml-4'>
+            <div className='mt-4 mx-8 w-fit'>
 
-                <div className='flex items-center gap-3 mt-4'>
-                    <input type="text" placeholder="Search tredify" className="p-2 w-full border border-secondary lg:w-48 max-w-xs " />
+                <div className='flex items-center gap-3 mt-4 mb-4'>
+                    <input type="text" placeholder="Search tredify" className="p-2 border border-secondary w-full  " />
                     <BsSearch className='text-2xl text-secondary'></BsSearch>
                 </div>
 
@@ -55,26 +55,41 @@ const Menu = () => {
                     <FaEdit className='text-3xl text-secondary' />
                     <p className=' font-semibold  md:text-lg'>Write a post</p>
                 </div> */}
-                <div className="flex items-center gap-3 mt-4">
-                    <BiNews className='text-3xl text-secondary' />
-                    <p className=' font-semibold  md:text-lg'>NewsFeed</p>
-                </div>
-                <div className="flex items-center gap-3 mt-4">
-                    <FaFacebookMessenger className='text-3xl text-secondary' />
-                    <p className=' font-semibold  md:text-lg'>Chats</p>
-                </div>
-                <div className="flex items-center gap-3 mt-4">
-                    <BsPeopleFill className='text-3xl text-secondary' />
-                    <p className=' font-semibold  md:text-lg'>Friends</p>
-                </div>
-                <div className="flex items-center gap-3 mt-4">
-                    <MdVideoLibrary className='text-3xl text-secondary' />
-                    <p className=' font-semibold  md:text-lg'>Videos</p>
-                </div>
-                <div className="flex items-center gap-3 mt-4">
-                    <MdBookmarks className='text-3xl text-secondary' />
-                    <p className=' font-semibold  md:text-lg'>Saved</p>
-                </div>
+                <Link className="flex items-center gap-3 py-[6px] px-3   w-48 rounded-md mt-2 border-[1px] border-cyan-500 ">
+                    <div className=" p-2 rounded-lg bg-blue-100">
+                        <BiNews className=" text-2xl text-[#0362fc]"></BiNews>
+                    </div>
+                    {/* <BiNews className='text-3xl text-secondary' /> */}
+                    <p className=' text-gray-700 font-[500]  md:text-lg'>NewsFeed</p>
+                </Link>
+                <Link className="flex items-center gap-3 py-[6px] px-3   w-48 rounded-md mt-2 border-[1px] border-cyan-500 ">
+                    <div className=" p-2 rounded-lg bg-cyan-100">
+                        <FaFacebookMessenger className=" text-2xl text-[#03fce3]"></FaFacebookMessenger>
+                    </div>
+                    {/* <FaFacebookMessenger className='text-3xl text-secondary' /> */}
+                    <p className=' text-gray-700 font-[500]  md:text-lg'>Chats</p>
+                </Link>
+                <Link className="flex items-center gap-3 py-[6px] px-3   w-48 rounded-md mt-2 border-[1px] border-cyan-500 ">
+                    <div className=" p-2 rounded-lg bg-yellow-100">
+                        <BsPeopleFill className=" text-2xl text-[#e3a936]"></BsPeopleFill>
+                    </div>
+                    {/* <BsPeopleFill className='text-3xl text-secondary' /> */}
+                    <p className=' text-gray-700 font-[500]  md:text-lg'>Friends</p>
+                </Link>
+                <Link className="flex items-center gap-3 py-[6px] px-3   w-48 rounded-md mt-2 border-[1px] border-cyan-500 ">
+                    <div className=" p-2 rounded-lg bg-red-100">
+                        <MdVideoLibrary className=" text-2xl text-[#fc0303]"></MdVideoLibrary>
+                    </div>
+                    {/* <MdVideoLibrary className='text-3xl text-secondary' /> */}
+                    <p className=' text-gray-700 font-[500]  md:text-lg'>Videos</p>
+                </Link>
+                <Link className="flex items-center gap-3 py-[6px] px-3   w-48 rounded-md mt-2 border-[1px] border-cyan-500 ">
+                    <div className=" p-2 rounded-lg bg-purple-100">
+                        <MdBookmarks className=" text-2xl text-[#8403fc]"></MdBookmarks>
+                    </div>
+                    {/* <MdBookmarks className='text-3xl text-secondary' /> */}
+                    <p className=' text-gray-700 font-[500]  md:text-lg'>Saved</p>
+                </Link>
             </div>
         </div>
     );
