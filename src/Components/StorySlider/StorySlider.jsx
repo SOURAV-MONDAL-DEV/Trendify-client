6724db2cc09db0f57ec09e6ff93eb4cc245408dd
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-08-08 19:54:08 +0600

    Create: story slider with design fixed

--- src/Components/StorySlider/StorySlider.jsx
+++ src/Components/StorySlider/StorySlider.jsx
@@ -4,50 +4,65 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { storyData } from "./HomeStory";
-import "./Story.css";
+// import "./Story.css";
 
 const Slider = () => {
   return (
-      <div className="max-w-[600px] mx-auto" >
-        <Swiper
-          modules={[Navigation, Pagination]}
-          navigation
-          spaceBetween={5}
-          slidesPerView={3}
-          breakpoints={{
-            600: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
-            1024: {
-              slidesPerView: 5,
-              spaceBetween: 15,
-            },
-            1500: {
-              slidesPerView: 6,
-              spaceBetween: 15,
-            },
-            1920: {
-              slidesPerView: 7,
-              spaceBetween: 15,
-            },
-          }}
-        >
-          {storyData.map((story, idx) => (
-            <SwiperSlide key={idx}>
-              <div className="storyCard relative" >
-                <img className="storyPhoto w-full h-180" src={story.imgUrl} alt=""></img>
-                <img className="userPhoto absolute bottom-[-15px] left-1/2 transform translate-x-[-50%] w-30 h-30 object-cover border-2 border-white rounded-6" src={story.imgUrl} alt=""></img>
+    <div className="max-w-full mx-auto mt-0 mb-2 lg:my-4">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        spaceBetween={5}
+        slidesPerView={3.5}
+        breakpoints={{
+          600: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 6,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+          },
+          1500: {
+            slidesPerView: 6,
+            spaceBetween: 15,
+          },
+          1920: {
+            slidesPerView: 7,
+            spaceBetween: 15,
+          },
+        }}
+      >
+        {storyData.map((story, idx) => (
+          <SwiperSlide key={idx}>
+            <div>
+              {/* <img
+                className="storyPhoto w-full h-[180px] object-cover"
+                src={story.imgUrl}
+                alt=""
+              ></img> */}
+              <div className=" relative">
+                <img
+                  className="storyPhoto w-full h-[180px] object-cover"
+                  src={story.imgUrl}
+                  alt=""
+                ></img>
+                <img
+                  className=" w-8 h-8 absolute bottom-[-15px] left-1/2 transform translate-x-[-50%] object-cover border-2 border-white rounded-6"
+                  src={story.imgUrl}
+                  alt=""
+                ></img>
               </div>
-              <div className="personName mt-20 text-sm">{story.personName}</div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+              <div className="personName mt-4 mx-auto text-center text-sm">{story.personName}</div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
